80f416fadee18f9a65d149736f604ffbf7a576fe
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2026-06-22 20:20:28 +0600

    modify route.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,13 @@ import userRoutes from "./user.routes.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to Apprelix API v1!"
+  });
+});
+
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 
